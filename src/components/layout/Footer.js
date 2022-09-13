5ea42155cb6f4324e743824f173d2b7fecb1dc10
luanfsouza <luanferreira7777@gmail.com>
2022-09-13 14:37:30 -0300

    ajustes

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,20 +1,32 @@
 import styles from './Footer.module.css'
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
-const a = FaFacebook
+import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from "react-icons/ai";
 function Footer(){
     return (
-        
-        <footer className={styles.footer}>
-            <ul className={styles.social_list}>
-                <li>{<FaFacebook/>}</li>
-                <li>{<FaInstagram/>}</li>
-                <li>{<FaLinkedin/>}</li>
-            </ul>
-            <p className={styles.copy_right}>
-                <span>Costs</span> &copy;2021
-            </p>
-            </footer>
-        
-    )
+      <footer className={styles.footer}>
+        <ul className={styles.social_list}>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/luan-ferreira-6b4a25181/"
+              target="_blank"
+            >
+              {<AiFillLinkedin />}
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/luanfsouza/costs" target="_blank">
+              {<AiFillGithub />}
+            </a>
+          </li>
+          <li>
+            <a href="https://is.gd/ltuM33" target="_blank">
+              {<AiOutlineMail />}
+            </a>
+          </li>
+        </ul>
+        <p className={styles.copy_right}>
+          <span>Costs</span> &copy;2021
+        </p>
+      </footer>
+    );
 }
 export default Footer
